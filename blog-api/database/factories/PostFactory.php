01315ef16445e75b7c96c7954cd95b9fa95c1f78
rin4ik <m.mirakhmad@gmail.com>
2022-02-28 15:01:52 +0500

    first commit

---
+++ blog-api/database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            // ''
+        ];
+    }
+}
