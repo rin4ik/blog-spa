f3d3d895b9eecf35e87cdfcbde5ae3d506c618af
rin4ik <m.mirakhmad@gmail.com>
2022-03-04 14:02:48 +0500

    post policy

--- blog-api/app/Http/Controllers/PostShowController.php
+++ blog-api/app/Http/Controllers/PostShowController.php
@@ -9,6 +9,7 @@ class PostShowController extends Controller
 {
     public function __invoke(Post $post)
     { 
+        $this->authorize('show', $post);
         return new PostEditResource($post);
     }
 }
